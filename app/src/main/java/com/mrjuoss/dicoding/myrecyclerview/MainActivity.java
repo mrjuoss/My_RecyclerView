3a2f06c1756b372b7b8470ad7521a65c9f94088b
mrjuoss <mrjuoss@gmail.com>
2019-06-15 10:25:23 +0700

    Add Menu Navigation

--- app/src/main/java/com/mrjuoss/dicoding/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/mrjuoss/dicoding/myrecyclerview/MainActivity.java
@@ -1,10 +1,13 @@
 package com.mrjuoss.dicoding.myrecyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -12,6 +15,29 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView rvCategory;
     private ArrayList<President> list = new ArrayList<>();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_list:
+
+                break;
+            case R.id.action_grid:
+
+                break;
+            case R.id.action_cardView:
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
